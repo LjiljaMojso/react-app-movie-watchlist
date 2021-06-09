a97f7686e3c97a9304226222f3dbde9a550946eb
LjiljaMojso <ljmojso@gmail.com>
2021-06-09 17:34:21 +0200

    create deployment

---
+++ src/Database.js
@@ -0,0 +1,9 @@
+//Firebase
+export const firebaseConfig = {
+    apiKey: "AIzaSyAAGnezp35ixKkNOg1SrH6ie6LlHrpryCM",
+    authDomain: "reactapp-moviewatchlist.firebaseapp.com",
+    projectId: "reactapp-moviewatchlist",
+    storageBucket: "reactapp-moviewatchlist.appspot.com",
+    messagingSenderId: "374423090822",
+    appId: "1:374423090822:web:f7f3df5ba5aa819d34c307"
+};
--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
+/* import { firebase } from "firebase";
+import { firebaseConfig } from './Database';
 
+firebase.initializeApp(firebaseConfig); */
 
 ReactDOM.render(<BrowserRouter><App color="light"/></BrowserRouter>,document.getElementById('root'));
 
